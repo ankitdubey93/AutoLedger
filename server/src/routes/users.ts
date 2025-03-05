4717993e2ecc4d2f55864dccf9296342e843f7fd
Ankit Dubey <ankit19933011@gmail.com>
2025-03-05 18:09:52 +0530

    Dashboard page after signin Added with a welcome message

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -62,6 +62,25 @@ router.post('/login', async (req: Request, res: Response): Promise<void> => {
     }
 });
 
+router.get('/:id', async (req: Request, res: Response): Promise<void> => {
+    try {
+       
+        const user = await User.findById(req.params.id);
+        if(!user) {
+            res.status(404).json({message: 'User not found.'});
+            return;
+        }
+        res.status(201).json(user);
+    } catch (error) {
+        console.error('Error fetching user by id: ', error);
+        res.status(500).json({message: 'Error fetching data: ', error});
+    }
+})
+
+
+
+// THis is a development level route only. Delete after development testing.
+
 
 router.get('/', async (req: Request, res: Response):Promise<void> => {
     const users = await User.find();
